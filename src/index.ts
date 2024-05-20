5655d46f2411b5af0f792313cc69f6bf5ecfa4e3
Gerben Rampaart <rampaart@gmail.com>
2024-05-20 16:26:57 +0200

    Changed to mod.ts. Using alpine.

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './lib-utilities.module.ts';
-export * from './util/AppFactory.ts';
-export * from './services/config/index.ts';
-export * from './services/http/index.ts';
-export * from './services/logger/index.ts';
-export * from './services/constants/index.ts';
-export * from '@nestjs/common';
